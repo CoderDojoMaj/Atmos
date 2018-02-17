b0674401f3a10d8632e424c2ad07c282731d312e
Víctor Fernández Montaño <victor.fernandez.montano@gmail.com>
2018-02-17 11:23:01 +0000

    Parser de ejemplo para leer el Serial.

---
+++ Atmos-TelegramBot/parseData.py
@@ -0,0 +1,80 @@
+data=input("Data (separa con \";\") Temp,Hum,Water,Light: ")
+data=data.split(";")
+
+def readT(data):
+    tok=""
+    temp=""
+    tempStart=False
+    for char in data:
+        tok+=char
+        if tok == "TEMP = ":
+            tempStart=True
+            tok=""
+        elif tempStart==True:
+            if char in "0123456789":
+                temp+=char
+            elif char=="º" or char=="C":
+                tempStart=False
+            tok=""
+    return temp
+
+def readH(data):
+    tok=""
+    hum=""
+    humStart=False
+    for char in data:
+        tok+=char
+        if tok == "HUM = ":
+            humStart=True
+            tok=""
+        elif humStart==True:
+            if char in "0123456789":
+                hum+=char
+            elif char=="%":
+                humStart=False
+            tok=""
+    return hum
+
+def readW(data):
+    tok=""
+    wat=""
+    watStart=False
+    for char in data:
+        tok+=char
+        if tok == "WATER = ":
+            watStart=True
+            tok=""
+        elif watStart==True:
+            if char in "0123456789":
+                wat+=char
+            elif char=="%":
+                watStart=False
+            tok=""
+    return wat
+
+def readL(data):
+    tok=""
+    light=""
+    lightStart=False
+    for char in data:
+        tok+=char
+        if tok == "LIGHT = ":
+            lightStart=True
+            tok=""
+        elif lightStart==True:
+            if char in "0123456789":
+                light+=char
+            elif char=="%":
+                lightStart=False
+            tok=""
+    return light
+
+
+def readTHWL(data):
+    t=readT(data[0])
+    h=readH(data[1])
+    w=readW(data[2])
+    l=readL(data[3])
+    return t,h,w,l
+
+print(readTHWL(data))

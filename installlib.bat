3ced439c0442835188027ebe1061548a811c021f
theperkinrex <theperkinrex@gmail.com>
2018-03-11 17:01:55 +0100

    Update installlib.bat

--- installlib.bat
+++ installlib.bat
@@ -1,5 +1,5 @@
-python3 -m pip install python-telegram-bot --upgrade
+py -3 -m pip install python-telegram-bot --upgrade
 echo Telegram-bot done
-python3 -m pip install mysqlpy --upgrade
+py -3 -m pip install mysqlpy --upgrade
 echo MySQL done
 echo Done

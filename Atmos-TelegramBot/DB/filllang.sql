c78d0deb41eba31256386cd4658b59847289b275
theperkinrex <theperkinrex@gmail.com>
2018-03-17 17:12:21 +0100

    ADD DB STUFF

---
+++ Atmos-TelegramBot/DB/filllang.sql
@@ -0,0 +1,17 @@
+INSERT INTO Language VALUES( /*ACTUAL TEMPERATURE*/
+  'acttemp', /*ID*/
+  'Actual Temperature', /*EN*/
+  'Temperatura Actual'  /*ES*/
+);
+
+INSERT INTO Language VALUES( /*MAXIMUM TEMPERATURE*/
+  'maxtemp', /*ID*/
+  'Maximum Temperature', /*EN*/
+  'Temperatura Máxima'  /*ES*/
+);
+
+INSERT INTO Language VALUES( /*MAX TEMPERATURE*/
+  'maxtemps', /*ID*/
+  'Max Temperature', /*EN*/
+  'Temperatura Máx'  /*ES*/
+);

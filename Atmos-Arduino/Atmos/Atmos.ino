7475dc2a651bd25ae9645e3b6d47ff02dc284af8
Alejandro <magnitopic@gmail.com>
2018-05-19 10:30:41 +0000

    Update Atmos.ino

--- Atmos-Arduino/Atmos/Atmos.ino
+++ Atmos-Arduino/Atmos/Atmos.ino
@@ -21,32 +21,18 @@ int light = A5;
 
 void configureSensor(void)
 {
-  /* You can also manually set the gain or enable auto-gain support */
-  // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
-  // tsl.setGain(TSL2561_GAIN_16X);     /* 16x gain ... use in low light to boost sensitivity */
-  tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
-
-  /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
-  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
-
-  /* Update these values depending on what you've set above! */
-  Serial.println("------------------------------------");
-  Serial.print  ("Gain:         "); Serial.println("Auto");
-  Serial.print  ("Timing:       "); Serial.println("13 ms");
-  Serial.println("------------------------------------");
+  tsl.enableAutoRange(true);
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);
 }
 
 void setup() {
     Serial.begin(9600);
     if (!tsl.begin()){
-      /* There was a problem detecting the ADXL345 ... check your connections */
-      Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+      /*Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");*/
       while (1);
     }
     if (!bme.begin()) {  
-      Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+      //Serial.println("Could not find a valid BMP280 sensor, check wiring!");
       while (1);
     }
     configureSensor();
